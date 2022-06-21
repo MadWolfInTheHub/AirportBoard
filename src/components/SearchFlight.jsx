f4295c33aba31718e045fc717325b494264c11c4
MadWolfInTheHub <krivenko.serhii@icloud.com>
2022-06-21 23:26:55 +0800

    test

--- src/components/SearchFlight.jsx
+++ src/components/SearchFlight.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react';
 import FlightsList from './FlightsList';
 import FligthsType from './FligthsType';
